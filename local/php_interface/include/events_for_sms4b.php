1f09c17f12ab4cc686024d84fb59b47d1345925b
Валентин Хорищук <khoris@rarus.ru>
2017-10-19 09:53:15 +0300

    "Fix and discribe sintacsis error"

--- local/php_interface/include/events_for_sms4b.php
+++ local/php_interface/include/events_for_sms4b.php
@@ -1,16 +1,16 @@
-<?
+<?php
 
 use Bitrix\Main\Type\DateTime;
 
-AddEventHandler("main", "OnBeforeEventAdd", "editOrderSMS");
+AddEventHandler('main', 'OnBeforeEventAdd', 'editOrderSMS');
 
 /**
- * @param $event
+ * @param $event_name
  * @param $site
  * @param $arFields
  * @return bool|void
  */
-function editOrdeпшеrSMS($event_name, $site, &$arFields)
+function editOrderSMS($event_name, $site, &$arFields)
 {
     if ($event_name !== 'SALE_NEW_ORDER') {
         return;
@@ -26,13 +26,18 @@ function editOrdeпшеrSMS($event_name, $site, &$arFields)
     $orderId = $arFields['ORDER_ID'];
 
     $arCode = array(
-        "ZIP" => 'ZIP',
-        "LOCATION" => 'LOCATION',
-        "ADDRESS" => 'ADDRESS',
+        'ZIP' => 'ZIP',
+        'LOCATION' => 'LOCATION',
+        'ADDRESS' => 'ADDRESS',
     );
-    $arFields["PHONE_TO"] = $SMS4B->is_phone($SMS4B->GetPhoneOrder($orderId, "s1"));
-    $arFields["ORDER_DATE"] = date("d-m-Y", strtotime($arFields["ORDER_DATE"]));
+    $arFields['PHONE_TO'] = $SMS4B->is_phone($SMS4B->GetPhoneOrder($orderId, 's1'));
+    $arFields['ORDER_DATE'] = date('d-m-Y', strtotime($arFields['ORDER_DATE']));
 
+    $orderIdReal = null;
+    // 2 потенциальные ошибки вшитые в битрикс $by => $order
+    // в D7 прямого подобного метода нет (если критично можно через getBasket попробовать)
+    $by = 'ID';
+    $order='DESC';
     $dbOrderList = CSaleOrder::GetList(
         array($by => $order),
         array('ACCOUNT_NUMBER' => $orderId),
@@ -54,30 +59,53 @@ function editOrdeпшеrSMS($event_name, $site, &$arFields)
         )
     );
     while ($arrOrder = $dbProps->Fetch()) {
-        $arFields[$arrOrder["CODE"]] = $arrOrder['VALUE'];
+        $arFields[$arrOrder['CODE']] = $arrOrder['VALUE'];
     }
 
-    if (!empty($arFields["LOCATION"])) {
-        $Location = CSaleLocation::GetByID($arFields["LOCATION"]);
-        $arFields["LOCATION"] = $Location["COUNTRY_NAME_ORIG"] . ' ' . $Location["REGION_NAME_ORIG"] . ' ' . $Location["CITY_NAME_ORIG"];
+    if (!empty($arFields['LOCATION'])) {
+        $Location = CSaleLocation::GetByID($arFields['LOCATION']);
+        $arFields['LOCATION'] = $Location['COUNTRY_NAME_ORIG'] . ' ' . $Location['REGION_NAME_ORIG'] . ' ' . $Location['CITY_NAME_ORIG'];
     }
 
     // Date Time block
+    /**
+     * @param $templSms4bUs
+     * @param $event_name
+     * @param $site
+     * @param $arFields
+     * @param string $admin
+     */
+    // Ошибка: Больше трех параметров в функции - плохая практика, я - плохой практикант.
+    /**
+     * @param $templSms4bUs
+     * @param $event_name
+     * @param $site
+     * @param $arFields
+     * @param string $admin
+     * @return bool
+     */
     function SendSmsToUser($templSms4bUs, $event_name, $site, $arFields, $admin = 'N')
     {
         if (!CModule::includeModule('rarus.sms4b')) {
             AddMessage2Log('не подключен модуль rarus.sms4b');
-            return;
+            return false;
         }
 
         global $SMS4B;
 
+        //Методы GetCurrentOption GetEventTemplate принадлежат объекту $SMS4B - шторм ошибся в ошибке (смотри dev)
         $sender = $SMS4B->GetCurrentOption('defsender', $site);
         $text = $SMS4B->GetEventTemplate($templSms4bUs . $event_name, $site);
 
+        // Ошибка - риск сбоя, из-за того что тип передаваемой переменной не определен - считаю можно проигнорировать
         foreach ($arFields as $k => $value) {
             $text['MESSAGE'] = str_replace('#' . $k . '#', $value, $text['MESSAGE']);
         }
+
+        //Здесь ошибки - метод is_phone GetPhoneOrder и GetAdminPhones не принадлежат объекту - но по факту принадлежит
+        //AddMessage2Log('<br> method_exists GetAdminPhones? - '.method_exists($SMS4B, 'GetAdminPhones'));
+        //AddMessage2Log('<br> method_exists GetPhoneOrder? - '.method_exists($SMS4B, 'GetPhoneOrder'));
+        //AddMessage2Log('<br> method_exists is_phone? - '.method_exists($SMS4B, 'is_phone'));
 
         if ($admin !== 'Y') {
             $phone_send_sms = $SMS4B->is_phone($SMS4B->GetPhoneOrder($arFields['ORDER_ID'], $site));
@@ -90,47 +118,40 @@ function editOrdeпшеrSMS($event_name, $site, &$arFields)
             return false;
         }
 
+        //Здесь ошибка метод Translit не принадлежит объекту - но по факту принадлежит
+        //AddMessage2Log('<br> method_exists Translit? - '.method_exists($SMS4B, 'Translit'));
+        // шторм  ошибся наверно потому что этот метод принадлежит родителю того класса, который создал метод, а не классу-создателю объекта
         $message = $SMS4B->use_translit === 'Y' ? $SMS4B->Translit($text['MESSAGE']) : $text['MESSAGE'];
 
         $startQuitPeriod = DateTime::createFromPhp(new \DateTime(START_QUITE_PERIOD));
         $endQuitPeriod = DateTime::createFromPhp(new \DateTime(END_QUITE_PERIOD));
 
         $nowTime = new DateTime();
-
-        if (is_object($nowTime)) {
-            AddMessage2Log('object DateTime - Y');
-        } else {
-            AddMessage2Log('not object DateTime - dont create');
-        }
+        $timeToSend = null;
 
         if (($startQuitPeriod <= $nowTime || $nowTime <= $endQuitPeriod) && ($startQuitPeriod > $endQuitPeriod)) {
-            AddMessage2Log('Quite period');
-            $timeToSend = $endQuitPeriod->add("1 day");
+            $timeToSend = $endQuitPeriod->add('1 day');
         } elseif (($startQuitPeriod <= $nowTime && $nowTime <= $endQuitPeriod) && ($startQuitPeriod < $endQuitPeriod)) {
-            ;
-            AddMessage2Log('Quite period');
             $timeToSend = $endQuitPeriod;
-        } else {
-            AddMessage2Log('not Quite period');
         }
 
         if (is_array($phone_send_sms)) {
             foreach ($phone_send_sms as $phone_num) {
-                $res = $SMS4B->SendSmsSaveGroup(array($phone_num => $message), $sender, $timeToSend, null, null, null,
-                    $arFields['ORDER_ID'],
-                    $event_name, 0);
+                //Здесь ошибка - SendSmsSaveGroup не принадлежит объекту $SMS4B  Method 'SendSmsSaveGroup' not found in less... Referenced method is not found in subject class.
+                // но по факту - принадлежит
+                //AddMessage2Log('method_exists? - '.method_exists($SMS4B, 'SendSmsSaveGroup'));
+                // шторм наверно ошибся потому что этот метод принадлежит родителю того класса, который создал метод, а не классу-создателю объекта
+                $SMS4B->SendSmsSaveGroup(array($phone_num => $message), $sender, $timeToSend, null, null, null,
+                    $arFields['ORDER_ID'], $event_name, 0);
             }
         } else {
-            $res = $SMS4B->SendSmsSaveGroup(array($phone_send_sms => $message), $sender, $timeToSend, null, null, null,
-                $arFields['ORDER_ID'],
-                $event_name, 0);
+            $SMS4B->SendSmsSaveGroup(array($phone_send_sms => $message), $sender, $timeToSend, null, null, null,
+                $arFields['ORDER_ID'], $event_name, 0);
         }
 
+        return true;
     }
 
     SendSmsToUser('SMS4B_', $event_name, $site, $arFields);
     SendSmsToUser('SMS4B_ADMIN_', $event_name, $site, $arFields, 'Y');
 }
-
-
-?>
